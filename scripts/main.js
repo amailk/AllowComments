7886afa5e477bffa21296f03aa919cb029970ee4
amailk <Ama@Ama.local>
2016-01-24 23:29:32 -0500

    kept comments only for note pages and styled.

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,21 @@ $(document).ready(function(){
     $("#showAboutMe").click(function(){
         $("#about").show("slow");
         $("#notes").hide("slow");
-        $("#next_steps").hide("slow");
+        $("#next-steps").hide("slow");
+        $("#comment-wrapper").hide("fast");
     });    
 
     $("#showNotes").click(function(){
         $("#about").hide("slow");
         $("#notes").show("slow");
-        $("#next_steps").hide("slow");
+        $("#next-steps").hide("slow");
+        $("#comment-wrapper").show("fast");
     });
 
     $("#showNextSteps").click(function(){
         $("#about").hide("slow");
         $("#notes").hide("slow");
-        $("#next_steps").show("slow");
+        $("#next-steps").show("slow");
+        $("#comment-wrapper").show("fast");
     });
-
 });
